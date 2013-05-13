dfd1378a2c67199df9feb11d84b9b1d5a7f1c84d
do <udo@maketank.net>
2013-05-13 17:41:20 +0200

    replace may receive filename as

--- manifests/defines/replace.pp
+++ manifests/defines/replace.pp
@@ -29,13 +29,16 @@
 #  		pattern => "^port (?!$port)[0-9]*",
 #  		replacement => "port $port"
 #  }  
-define replace($file, $pattern, $replacement) {
+define replace($file='', $pattern, $replacement) {
 	$pattern_no_slashes = regsubst($pattern, '/', '\\/', 'G', 'U')
 	$replacement_no_slashes = regsubst($replacement, '/', '\\/', 'G', 'U')
-
-	exec { "replace_${pattern}_${file}":
-		command => "/usr/bin/perl -pi -e 's/${pattern_no_slashes}/${replacement_no_slashes}/' '${file}'",
-		onlyif => "/usr/bin/perl -ne 'BEGIN { \$ret = 1; } \$ret = 0 if /${pattern_no_slashes}/ && ! /\\Q${replacement_no_slashes}\\E/; END { exit \$ret; }' '${file}'",
+  $file_r = $file ? {
+    '' => $title,
+    default => $name,
+  }
+	exec { "replace_${pattern}_${file_r}":
+		command => "/usr/bin/perl -pi -e 's/${pattern_no_slashes}/${replacement_no_slashes}/' '${file_r}'",
+		onlyif => "/usr/bin/perl -ne 'BEGIN { \$ret = 1; } \$ret = 0 if /${pattern_no_slashes}/ && ! /\\Q${replacement_no_slashes}\\E/; END { exit \$ret; }' '${file_r}'",
 		alias => "exec_$name",
 	}
 }
