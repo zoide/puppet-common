f70003a89316e37eea6defc6b78ef4e482f9953a
Udo Waechter <u.waechter@mysportgroup.de>
2013-09-05 11:23:37 +0200

    better way of finding primarynetif

--- lib/facter/primary_netif.rb
+++ lib/facter/primary_netif.rb
@@ -37,7 +37,7 @@ Facter.add(:internal_netif) do
   confine :kernel => %w{Linux}
   setcode do
     iface = ""
-    output = %x{/sbin/ip a s to '10.0.0.0/8' |head -1 |awk '/:/ {print $2}' |sed s/://}.chomp
+    output = %x{/sbin/ip a s to '10.0.0.0/8' |head -1 |awk '/:/ {print $2}'|sed -e s/:// -e s/@.*//}.chomp
     if output != ""
       output
     else
