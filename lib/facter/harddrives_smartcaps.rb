02c658468c75fca8cba7fbb6a01512472723bafc
do <udo@maketank.net>
2014-03-19 20:57:08 +0100

    ruby 1.9

--- lib/facter/harddrives_smartcaps.rb
+++ lib/facter/harddrives_smartcaps.rb
@@ -14,7 +14,7 @@ if File.exists?(smartctl)
   ignorelines = "=== START OF READ SMART DATA SECTION ===|SMART Attributes Data Structure revision number|Vendor Specific SMART Attributes with Thresholds:|ID#\s+ATTRIBUTE_NAME\s+FLAG"
   smart_drvs = []
   if Facter.value(:harddrives) && Facter.value(:harddrives) != ""
-    Facter.value(:harddrives).split(",").each { |drive|
+    Facter.value(:harddrives).split(",").each{ |drive|
       next if drive =~ /^da/
       drvcaps = []
       %x{#{smartctl} -A /dev/#{drive} 2>/dev/null|sed 1,3d}.chomp.each_line { |line|
@@ -38,12 +38,12 @@ if File.exists?(smartctl)
 
   #now for all 3ware drives
   if Facter.value(:harddrives_3ware) && Facter.value(:harddrives_3ware) != ""
-    Facter.value(:harddrives_3ware).split(",").each { |drive|
+    Facter.value(:harddrives_3ware).split(",").each_line { |drive|
       drvcaps = []
       cntdrv = drive.split("_")
       controller = cntdrv[1][1,cntdrv[1].length]
       drv = cntdrv[2][1,cntdrv[2].length]
-      %x{#{smartctl} -d 3ware,#{drv} -A /dev/twa#{controller} 2>/dev/null|sed 1,3d}.chomp.each { |line|
+      %x{#{smartctl} -d 3ware,#{drv} -A /dev/twa#{controller} 2>/dev/null|sed 1,3d}.chomp.each_line { |line|
         next if line =~ /#{ignorelines}/
         vals = line.split(" ")
         if (Float(vals[0]) rescue false)
@@ -63,12 +63,12 @@ if File.exists?(smartctl)
   end
 
   if Facter.value(:has_aacraid)
-    Dir.glob("/dev/sg*").each{ |drive|
+    Dir.glob("/dev/sg*").each { |drive|
       %x{#{smartctl} -q silent -d sat -c #{drive} 2>/dev/null}
       if $?.exitstatus == 0
         drv = File.basename(drive)
         drvcaps = []
-        %x{#{smartctl} -d sat -A #{drive} 2>/dev/null|sed 1,3d}.chomp.each { |line|
+        %x{#{smartctl} -d sat -A #{drive} 2>/dev/null|sed 1,3d}.chomp.each_line { |line|
           next if line =~ /#{ignorelines}/
           vals = line.split(" ")
           if (Float(vals[0]) rescue false)
